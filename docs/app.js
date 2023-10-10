db19f292bf3a3ec6c588b5290103e6960e179a2d
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-10-10 15:43:28 -0500

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,21 @@
-bas
+baseDatos=localStorage.getItem("sistemaLogin")
+
+if(!baseDatos){
+    cargarDatosDB()
+}
+function cargarDatosDB(){
+    baseDatos = {
+        "felipe_rivas@prueba.com":{
+            contraseña:"felipe123"
+        },
+        "diana_araujo@prueba.com":{
+            contraseña:"diana123"
+        },
+        "daniel_jojoa@prueba-com":{
+            contraseña:"daniel123"
+            
+        }
+    }
+    
+}
+alert("Conectado");
